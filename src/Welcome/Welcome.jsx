6fbdac351b0e31a029d478ff50f9152ef540419e
Sintija <sintija.birgele@accenture.com>
2019-06-18 21:47:48 +0200

    update

--- src/Welcome/Welcome.jsx
+++ src/Welcome/Welcome.jsx
@@ -277,7 +277,7 @@ class Welcome extends React.Component{
             {isMobile && <div id="remotes" className="row">
               {<div className="col-md-6 ">
                 <div className="videoContainer" id="videoContainer">
-                  <video id="selfVideo" onContextMenu={()=> {return false} } muted playsInline controls="true"></video>
+                  <video id="selfVideo" onContextMenu={()=> {return false} } muted playsInline controls={true}></video>
                   <meter id="localVolume" className="volume" min="-45" max="-20" high="-25" low="-40"></meter>
                 </div>
               </div>}
