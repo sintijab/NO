f1e34eeff972326bf75ba027f9173452da789017
cynthiabirgele <sintija_birgele@inbox.lv>
2020-03-15 13:52:26 +0100

    style description

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import * as imgAboutSrc from '../images/68476430_608275159696277_7376439703328784384_o.png'
 import * as logoPost from '../images/logo.jpg'
+import { aboutText } from './about'
 
 class About extends React.Component {
 
@@ -12,9 +13,11 @@ class About extends React.Component {
       activeSections: [],
       isMobile: window.innerWidth < 800,
       toggleNavigation: window.innerWidth > 800 && true,
+      activateScroll: false,
     }
     this.filterSections = this.filterSections.bind(this)
     this.returnPrevState = this.returnPrevState.bind(this)
+    this.handleScroll = this.handleScroll.bind(this)
 
     const _this = this
     axios.get(`https://api.cosmicjs.com/v1/c61d0730-8187-11e9-9862-534a432d9a60/objects`, {
@@ -66,21 +69,42 @@ class About extends React.Component {
     }
   }
 
+  handleScroll() {
+    this.setState({ activateScroll: true })
+  }
+
   render() {
-    const { activeSections, toggleNavigation, isMobile } = this.state
+    const { activeSections, toggleNavigation, isMobile, activateScroll } = this.state
     let feed = <div />
     if (activeSections.length) {
       feed = activeSections.map(item => {
-        const feedItemClassName = item.metadata.navigation === 'about' ? 'NO_about-feed-item-intro' : 'NO__about-feed-item'
+        if ( item.metadata.navigation === 'about') {
+          let paragraphs = Math.random() * (aboutText.length - aboutText.length / 2) + aboutText.length / 2
+          Math.floor(paragraphs)
+          let text = aboutText.map((item) => <p>{item}</p>)
+          let feed = []
+          //eslint-disable-next-line
+          for (let i = 0; i <= aboutText.length; i++) {
+            const divStyle = {
+              fontWeight: Math.random() * (700 - 400) + 400,
+              color: (Math.random() * (3 - 1) + 1) < 1.5 ? '#ff0000' : '#000000',
+            }
+            feed.push(<div className='NO_about-feed-item-intro' style={divStyle}>{text[Math.floor(Math.random() * Math.floor(aboutText.length))]}</div>)
+          }
+          if (activateScroll) {
+            this.setState({ activateScroll: false })
+          }
+          return feed
+        }
         return (
-          <div className={feedItemClassName}>
+          <div className='NO__about-feed-item'>
             {!!item.content && <div dangerouslySetInnerHTML={{__html: item.content}} />}
           </div>
         )
       })
     }
       return (
-        <div className="NO__about-page" >
+        <div className="NO__about-page" onScroll={this.handleScroll}>
         <img className='NO__dot NO__about' src={imgAboutSrc} onClick={isMobile ? this.returnPrevState : () => window.close()}/>
         <a href='/00000'><img className='NO__dot' src={logoPost} onClick={this.viewMode} /></a>
           {toggleNavigation &&
