ee137e9d72ed4ab1886a6647af4e634964bebca1
cynthiabirgele <sintija_birgele@inbox.lv>
2020-03-20 14:48:54 +0100

    update

--- server.js
+++ server.js
@@ -5,5 +5,7 @@ const app = express()
 
 app.use(express.static(path.join(__dirname, 'build')))
 const port = process.env.PORT || 3000
-
+app.get('*', ((req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+}))
 app.listen(port, () => console.log(`Listening on ${port}`))
